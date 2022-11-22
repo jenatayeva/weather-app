cbe8eee7f13f2191e8f5f2ef43801873b48c3a84
jenatayeva <atayevajennet1126@gmail.com>
2022-11-22 15:06:05 +0500

    Untrack files in .gitignore

--- app.js
+++
@@ -1,9 +0,0 @@
-const request = require('request');
-require('dotenv').config();
-
-const url = 'http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.APP_ID}'
-
-request({ url: url }, (error, response) => {
-  const data =  JSON.parse(response.body)
-  console.log(data)
-})
